eb4764eb4bf26ceae313df75d0c755985f8a2810
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2019-07-20 09:54:05 +0800

    resolved

---
+++ src/java/main/school2019/Solution_11.java
@@ -0,0 +1,55 @@
+package main.school2019;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//丰收
+//二分查找
+public class Solution_11 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        String[] strs = bf.readLine().split(" ");
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = Integer.parseInt(strs[i]);
+        }
+        int m  = Integer.parseInt(bf.readLine());
+        strs = bf.readLine().split(" ");
+        int[] queryArray = new int[m];
+        for(int i = 0; i < m; i++){
+            queryArray[i] = Integer.parseInt(strs[i]);
+        }
+
+        for(int i = 1; i < n; i++){
+            array[i] = array[i - 1] + array[i];
+        }
+
+        for(int i = 0; i < m; i++){
+            int tmp = queryArray[i];
+            int left = 0;
+            int right = n -1;
+            while(left < right){
+                int mid = (left + right) / 2;
+                if(array[mid] > tmp){
+                    right = mid - 1;
+                }else if(array[mid] < tmp){
+                    left = mid + 1;
+                }else{
+                    System.out.println(mid + 1);
+                    break;
+                }
+            }
+
+            if(left >= right){
+                if(array[left] < tmp){
+                    left ++;
+                }
+                System.out.println(left + 1);
+            }
+        }
+    }
+}
---
+++ src/java/main/school2019/Solution_13.java
@@ -0,0 +1,48 @@
+package main.school2019;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//表达式求值
+public class Solution_13 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = bf.readLine().split(" ");
+        int a = Integer.parseInt(strs[0]);
+        int b = Integer.parseInt(strs[1]);
+        int c = Integer.parseInt(strs[2]);
+
+        if(a > 0 && b > 0 && c > 0){
+            if(a == 1 && b == 1 && c == 1){
+                System.out.println(3);
+            }else if(a == 1 || b == 1 || c == 1){
+                int max = 0;
+                if((a + b) * c > max){
+                    max = (a + b) * c;
+                }
+                if((a + c) * b > max){
+                    max = (a + c) * b;
+                }
+                if((b + c) * a > max){
+                    max = (b + c) * a;
+                }
+                System.out.println(max);
+            }else{
+                System.out.println(a * b * c);
+            }
+
+        }else if(a * b * c > 0){
+            if(a == 1 || b == 1 || c == 1){
+                System.out.println(a * b * c + 1);
+            }else{
+                System.out.println(a * b * c);
+            }
+        }else if(a * b > 0){
+            System.out.println(a + b + c);
+        }
+
+    }
+}
