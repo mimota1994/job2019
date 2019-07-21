b41f43349d3f6471dcb7dcfcf45f5c5f336cfbdf
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2019-07-21 19:22:33 +0800

    回文子串
    可以试试拉马车

--- src/java/main/school2019/kuaishou/Solution_29.java
+++ src/java/main/school2019/kuaishou/Solution_29.java
@@ -7,7 +7,43 @@ import java.util.Scanner;
 //回文子串
 public class Solution_29 {
     public static void main(String[] args) throws IOException {
-        
+        Scanner sc = new Scanner(System.in);
+        char[] array = sc.next().toCharArray();
+
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+
+            int left = i;
+            int right = i;
+            while(left >= 0 && right <= array.length -1){
+                if(array[left] == array[right]){
+                    sum ++;
+                    left --;
+                    right ++;
+                }else{
+                    break;
+                }
+            }
+        }
+
+        for(int i =0, j = 1; j < array.length; i ++, j++){
+            if(array[i] == array[j]){
+                int left = i;
+                int right = j;
+                while(left >= 0 && right <= array.length -1){
+                    if(array[left] == array[right]){
+                        sum ++;
+                        left --;
+                        right ++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(sum);
+
     }
 
 }
