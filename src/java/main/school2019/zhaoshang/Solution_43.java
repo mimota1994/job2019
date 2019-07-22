083baf972e279470420b8de30d5bb95ebd0b333f
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2019-07-22 21:29:03 +0800

    员工考勤记录
    递归
    其实用数学计算就可以了
    nC2 + nC1 + nC0

---
+++ src/java/main/school2019/zhaoshang/Solution_43.java
@@ -0,0 +1,24 @@
+package main.school2019.zhaoshang;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//员工考勤
+public class Solution_43 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = helper(2, n - 2) + helper(1, n - 1) + helper(0, n);
+        System.out.println(m);
+
+    }
+
+    public static int helper(int i, int j){
+        if(i <= 0 || j <= 0){
+            return 1;
+        }else{
+            return helper(i - 1, j) + helper(i, j - 1);
+        }
+    }
+}
