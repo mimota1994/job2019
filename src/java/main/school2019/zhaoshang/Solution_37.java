ffff5f7e40860fd684de2877badaa4fcc52c37b0
mimota <1643160505@qq.com>
2019-07-22 13:28:20 +0800

    冒泡问题

---
+++ src/java/main/school2019/zhaoshang/Solution_37.java
@@ -0,0 +1,29 @@
+package main.school2019.zhaoshang;
+
+import java.util.Scanner;
+
+//鸡鸭分类问题
+public class Solution_37 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		char[] array = sc.next().toCharArray();
+		
+		int num = 0;
+		for(int i = array.length - 2; i >= 0; i --) {
+			for(int j = i; j <= array.length - 2; j++) {
+				if(array[j] == 'D' && array[j + 1] == 'C') {
+					char tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
+					num ++;
+				}else {
+					break;
+				}
+			}
+		}
+		System.out.println(num);
+	}
+
+}
