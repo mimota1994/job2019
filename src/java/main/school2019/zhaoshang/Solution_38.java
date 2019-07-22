036c3a13cfacb69236c1a1e2dcfece7d624161c2
mimota <1643160505@qq.com>
2019-07-22 14:32:14 +0800

    dp

---
+++ src/java/main/school2019/zhaoshang/Solution_38.java
@@ -0,0 +1,34 @@
+package main.school2019.zhaoshang;
+
+import java.util.Scanner;
+
+//比特币买入卖出
+public class Solution_38 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] strs = sc.nextLine().split(" ");
+		
+		int[] array = new int[strs.length];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = Integer.parseInt(strs[i]);
+		}
+		
+		int[] dp = new int[array.length];
+		dp[1] = array[0];
+		for(int i = 2; i < dp.length; i++) {
+			dp[i] = Math.min(dp[i - 1], array[i - 1]);
+		}
+		
+		int max = 0;
+		for(int i = 1; i < array.length; i++) {
+			int tmp = array[i] - dp[i];
+			if(tmp > max) {
+				max = tmp;
+			}
+		}
+		System.out.println(max);
+		
+	}
+
+}
