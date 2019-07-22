b2d7ea622878e7fe770d9aadb4e78871538545ca
mimota <1643160505@qq.com>
2019-07-22 14:31:54 +0800

    余数和商，用余数判断是否循环

--- src/java/main/school2019/kuaishou/Solution_35.java
+++ src/java/main/school2019/kuaishou/Solution_35.java
@@ -11,13 +11,61 @@ import java.util.Scanner;
 //a/b
 public class Solution_35 {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         
-        int m = (int)Math.ceil((Math.sqrt(1 + 8 * n) - 1) / 2);
-        
-        System.out.println(m);
-  
-
+    	Scanner sc = new Scanner(System.in);
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	
+    	if(a % b == 0) {
+    		System.out.println(a / b);
+    		return;
+    	}
+    	
+    	int zhen = a / b;
+    	a = a % b;
+    	
+    	List<Integer> list = new ArrayList();
+    	Map<Integer, Integer> map = new HashMap<>();
+    	int shan = 0;
+    	int yu = 0;
+    	
+    	boolean isXunhuan = true;
+    	int index = 0;
+    	while(true) {
+    		shan = a / b;
+    		list.add(shan);
+    		yu = a % b;
+    		if(yu == 0) {
+    			isXunhuan = false;
+    			break;
+    		}
+    		if(map.containsKey(yu)) {
+    			isXunhuan = true;
+    			index = map.get(yu);
+    			break;
+    		}else {
+    			map.put(yu, list.size());
+    		}
+    		a = yu * 10;
+    	}
+    	
+    	String str = "" + zhen + ".";
+    	if(isXunhuan) {
+    		for(int i = 1; i < index; i++) {
+    			str += list.get(i);
+    		}
+    		str += "(";
+    		for(int i = index; i < list.size(); i++) {
+    			str += list.get(i);
+    		}
+    		str += ")";
+    	}else {
+    		for(int i = 1; i < list.size(); i++) {
+    			str += list.get(i);
+    		}
+    	}
+    	
+    	System.out.println(str);
+    	
     }
 }
