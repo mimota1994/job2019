a1bd27c63f6f259ca589ca9eeaaf74828bc63810
mimota <1643160505@qq.com>
2019-07-29 21:53:39 +0800

    方格走法 dp

---
+++ src/java/main/school2019Test/zhaoshang/Solution_52.java
@@ -0,0 +1,31 @@
+package main.school2019Test.zhaoshang;
+
+import java.util.Scanner;
+
+//方格走法
+//数据有点问题，需要+1
+public class Solution_52 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt() + 1;
+        int y = sc.nextInt() + 1;
+        int[][] matrix = new int[x][y];
+        for(int i = 0; i < x; i ++){
+            matrix[i][0] = 1;
+        }
+
+        for(int i = 0; i < y; i ++){
+            matrix[0][i] = 1;
+        }
+
+        for(int i = 1; i < x; i ++){
+            for(int j = 1; j < y; j++){
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+            }
+        }
+
+        System.out.println(matrix[x - 1][y - 1]);
+
+    }
+}
