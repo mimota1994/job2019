a06dcb34027e3c34fdd6a41c9417ce0d62b13f1d
huangzhigang <1643160505@qq.com>
2019-08-06 10:19:48 +0800

    最小立方数之和 dp解法

--- src/java/main/school2019Test/xiaomi/Solution_78.java
+++ src/java/main/school2019Test/xiaomi/Solution_78.java
@@ -6,29 +6,17 @@ import java.util.Scanner;
 /**
  * 最小立方数之和
  * 拆为立方数29825 结果为9,应该为4
+ * 960299, dp 结果为5，应该为4
+ * 更改了一下，可以有所优化
  *
  */
 public class Solution_78 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        int n = k;
-        int sum = 0;
-        while(n > 1){
-            int tmp = (int)Math.pow(n, 1.0 / 3);
-            tmp = tmp * tmp * tmp;
-            sum += (n / tmp);
-            n = n % tmp;
-        }
+        int n = sc.nextInt();
 
-        if(n == 1){
-            sum ++;
-        }
-        System.out.println(sum);
-
-
-        System.out.println(helper(k));
+        System.out.println(dpHelper(n));
 
     }
 
@@ -46,5 +34,26 @@ public class Solution_78 {
             }
         }
         return min;
+    }
+
+    public static int dpHelper(int n){
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for(int i = 1; i <= n; i++){
+            dp[i] = Integer.MAX_VALUE;
+            int a = (int)Math.pow(n, 1.0 / 3);
+            if(a * a * a == i){
+                dp[i] = 1;
+                continue;
+            }
+            for(int j = 1; j * j * j <= i; j++){
+                int tmp = dp[i - j *j * j];
+                if((tmp + 1) < dp[i]){
+                    dp[i] = tmp + 1;
+                }
+            }
+        }
+        return dp[n];
     }
 }
