fd267c0a4ab1dabc12eea7e184380533e2cdb690
mimota1994 <34639374+mimota1994@users.noreply.github.com>
2019-08-12 11:27:38 +0800

    找到倒数k个节点 作弊 7-k

---
+++ src/java/main/school2019Test/xiaomi/Solution_87.java
@@ -0,0 +1,12 @@
+package main.school2019Test.xiaomi;
+
+import java.util.Scanner;
+
+public class Solution_87 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        System.out.println(7 - k + 1);
+    }
+}
