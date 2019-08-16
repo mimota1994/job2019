8b41d9b556df23d5c3d933c0e84607245d6e1ce1
huangzhigang <1643160505@qq.com>
2019-08-16 17:10:21 +0800

    加入dfs和bfs

--- src/java/main/leecode/Main207.java
+++ src/java/main/leecode/Main207.java
@@ -2,6 +2,7 @@ package main.leecode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 现在你总共有 n 门课需要选，记为 0 到 n-1。
@@ -12,8 +13,11 @@ import java.util.List;
  * 拓扑排序，暴力，n3，超时
  *
  * solution2：
- * 拓扑排序，利用队列保存需要执行，也就是入度为0的元素，数组记录每个元素的入度，动态更新数值
+ * 拓扑排序 bfs，利用队列保存需要执行，也就是入度为0的元素，数组记录每个元素的入度，动态更新数值
  * 假如存在新的度为0的的元素，加入队列中
+ *
+ * solution3:
+ * dpf,利用栈保存路径
 
  */
 public class Main207 {
@@ -21,7 +25,7 @@ public class Main207 {
     public static void main(String[] args) {
         int numC = 2;
         int[][] pre = new int[][]{new int[]{1,0}};
-        System.out.println(canFinish2(numC, pre));
+//        System.out.println(canFinish2(numC, pre));
     }
 
     public boolean canFinish1(int numCourses, int[][] prerequisites) {
@@ -71,7 +75,13 @@ public class Main207 {
         return true;
     }
 
-    public static boolean canFinish2(int numCourses, int[][] prerequisites){
+    /**
+     * bfs
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish2(int numCourses, int[][] prerequisites){
         int[] v = new int[numCourses];  //保存出度，入度由矩阵保存
         int[][] matrix = new int[numCourses][numCourses];
         for(int i = 0; i < prerequisites.length; i++){
@@ -106,4 +116,70 @@ public class Main207 {
         }
         return true;
     }
+
+    /**
+     * dfs
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish3(int numCourses, int[][] prerequisites){
+
+        int[] status = new int[numCourses];
+        int[][] matrix = new int[numCourses][numCourses];
+        for(int i = 0; i < prerequisites.length; i++){
+            matrix[prerequisites[i][1]][prerequisites[i][0]] = 1;
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        while(!stack.isEmpty()){
+            int num = stack.peek();
+            if(status[num] == -1){
+                return false;
+            }else{
+                status[num] = -1;
+            }
+            int i = 0;
+            for( ; i < numCourses; i++){
+                if(matrix[num][i] == 1){
+                    stack.push(i);
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+//    private boolean dfs(int[][] adjacency, int[] flags, int i) {
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(i);
+//        flags[i] = 1;
+//        while(!stack.isEmpty()){
+//            int num = stack.peek();
+//
+//            int j = 0;
+//            for(; j < adjacency.length; j++){
+//                if(adjacency[num][j] == 1 && flags[j] == 0){
+//                    stack.push(j);
+//                    break;
+//                }
+//            }
+//
+//        }
+//
+//        return true;
+//    }
+
+    private boolean dfs(int[][] adjacency, int[] flags, int i) {
+        if(flags[i] == -1) return true;
+        flags[i] = 1;
+        for(int j = 0; j < adjacency.length; j++) {
+            if(adjacency[i][j] == 1 && !dfs(adjacency, flags, j)) return false;
+        }
+        flags[i] = -1;
+        return true;
+    }
+
+
 }
